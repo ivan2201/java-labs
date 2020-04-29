d782f8cace8a3c0d11b60ff7e12e28b9ae01321c
ivan2201 <i.kotov2000@gmail.com>
2020-04-29 11:48:20 +0300

    begin lab5

--- lab5/src/ru/baho/Application.java
+++ lab5/src/ru/baho/Application.java
@@ -1,34 +1,42 @@
 package ru.baho;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.scene.*;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
-import javax.swing.text.html.ListView;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Stack;
 
 public class Application extends javafx.application.Application {
 
     Store store;
     Statement db;
+    ObservableList productsViews;
     class ProductViews extends HBox
     {
+        Alert badFormatCost = new Alert(Alert.AlertType.ERROR);
         Label m_title = new Label();
         Label m_prodId = new Label();
         Label m_id = new Label();
         Label m_cost = new Label();
-
         public ProductViews(String title, String id, String prodId, String cost)
         {
             super();
+            badFormatCost.setTitle("Error uncorrect value");
+            badFormatCost.setHeaderText("this value has uncorrect format");
+            badFormatCost.setContentText(null);
             EventHandler<MouseEvent> event = new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
@@ -38,13 +46,23 @@ public class Application extends javafx.application.Application {
                     dialog.setContentText("Please enter new price:");
                     Optional<String> result = dialog.showAndWait();
                     if (result.isPresent()){
-                        m_cost.setText(result.get());
-
+                        try {
+                            store.setPrice(m_title.getText(), Long.parseLong(result.get()));
+                            m_cost.setText(result.get());
+                        } catch (Exception ex)
+                        {
+                            badFormatCost.showAndWait();
+                        }
                     }
 
                     result.ifPresent(name -> {
-                        m_cost.setText(name);
-
+                        try {
+                            store.setPrice(m_title.getText(), Long.parseLong(result.get()));
+                            m_cost.setText(name);
+                        } catch (Exception ex)
+                        {
+                            badFormatCost.showAndWait();
+                        }
                     });
                 }
             };
@@ -67,14 +85,43 @@ public class Application extends javafx.application.Application {
         db = Main.statement;
         if (db == null) return;
         store = new Store(db);
+        store.createTables();
+        store.fillTable(10);
         stage.setTitle("DBApplication");
         Text text = new Text("Store");
-        ListView producctList = new ListView();
+        productsViews = getProductViews();
+        ListView<ProductViews> productList = new ListView<ProductViews>(productsViews);
         Group root = new Group();
         StackPane pane = new StackPane();
         ObservableList list = root.getChildren();
+        list.add(productList);
         Scene scene = new Scene(root, 800,600);
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(new javafx.event.EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                store.deleteTables();
+            }
+        });
+    }
+
+    public ObservableList<ProductViews> getProductViews()
+    {
+        ArrayList<ProductViews> result = new ArrayList<ProductViews>();
+        ResultSet set = store.getAllProducts();
+        try {
+            while (set.next()) {
+                result.add(new ProductViews(set.getString(Store.POSITION_TITLE),
+                        set.getString(Store.POSITION_ID),set.getString(Store.POSITION_PRODID),
+                        set.getString(Store.POSITION_COST)));
+            }
+        } catch (Exception ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("error while getting");
+            alert.setContentText(ex.toString());
+            alert.showAndWait();
+        }
+        return FXCollections.observableArrayList(result);
     }
 }
---
+++ lab5/src/ru/baho/Main.java
@@ -0,0 +1,40 @@
+package ru.baho;
+import java.sql.*;
+
+public class Main {
+    static Connection db = null;
+    static Statement statement = null;
+    static final String DB_URL = "jdbc:postgresql://127.0.0.1:5432/java_lab_4_db?serverTimezone=Europe/Moscow&useSSL=false";
+    static final String USER = "baho";
+    static final String PASS = "KillerOfMyMind29";
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
+            db = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("Connection succesfull!");
+        } catch (Exception ex) {
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+        }
+        if (db != null) {
+            System.out.println("You successfully connected to database now");
+        } else {
+            System.out.println("Failed to make connection to database");
+        }
+        try {
+            statement = db.createStatement();
+            //(new Console()).run();
+        } catch (Exception ex) {
+    }
+        closeDB();
+    }
+    private static void closeDB()
+    {
+        try {
+            db.close();
+        } catch (Exception ex)
+        {
+            System.out.println("Failed disconnection to database");
+        }
+    }
+}
